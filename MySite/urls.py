c724d26bdf7763f47175b910d51f04721d83bd0d
victor <vmalvaradop0@gmail.com>
2024-04-10 17:41:09 -0600

    se realizo el responsive de la pagina y se le dio funcionalidad al boton de la app

--- MySite/urls.py
+++ MySite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("food/", include("food.urls")),
+    path("food/", include("food.urls", namespace="mi_app")),
 ]
