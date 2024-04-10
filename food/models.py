6a4d50cdc69db492950fc730a46685017718025f
victor <vmalvaradop0@gmail.com>
2024-04-10 16:43:40 -0600

    se agrego la barra de navegacion y se empezaron a hacer los flujos

--- food/models.py
+++ food/models.py
@@ -6,7 +6,7 @@ class item(models.Model):
     def __str__(self):
         return self.item_name
 
-
     item_name = models.CharField(max_length=200)
     item_description = models.CharField(max_length=500)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=700, default = 'https://theme-assets.getbento.com/sensei/5271791.sensei/assets/images/catering-item-placeholder-704x520.png' )
