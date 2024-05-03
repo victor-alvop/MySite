d571960e4fa21f8bc62aca7a30e4b2e7ac778603
victor <vmalvaradop0@gmail.com>
2024-05-03 15:12:21 -0600

    Funcionalidad de login

--- MySite/urls.py
+++ MySite/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as authentication_views 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("food/", include("food.urls", namespace="mi_app")),
-    path("register/", user_views.create_user, name='create_user')
+    path("register/", user_views.create_user, name="create_user"),
+    path("login/", authentication_views.LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", authentication_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
+    path("profile/", user_views.profile_page,name='profile')
 ]
--- users/views.py
+++ users/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def create_user(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}')
             return redirect('food:index')
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
         
     return render(request, 'users/register.html', {'form':form})
+
+
+# def logout_view(request):
+#     logout(request)
+#     # Redirige a la página de inicio o a donde desees que el usuario vaya después del logout.
+#     return redirect('food:index')
+
+@login_required
+def profile_page(request):
+    return render(request, 'users/profile.html')
