8408e6b48712c85fa9ef8136a80e4af9b8c011be
victor <vmalvaradop0@gmail.com>
2025-05-01 13:32:52 -0600

    Port configuration

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Usar el puerto de la variable de entorno 'PORT' que asigna Railway, o 5000 por defecto
+    port = int(os.getenv('PORT', 5000))
+
+    # Ejecutar la aplicación Flask en el puerto y dirección correcta
+    app.run(host='0.0.0.0', port=port, debug=True)
