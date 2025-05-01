1d0058a33fd6459cff4a510c052f2e43f6a28aca
victor <vmalvaradop0@gmail.com>
2025-05-01 13:12:09 -0600

    Create Procfile file

--- app/database/db_target.py
+++ app/database/db_target.py
@@ -4,7 +4,6 @@ from app import db
 class Summary(db.Model):
     __tablename__ = 'summary'
     __bind_key__ = 'DB_TARGET_URL'
-    __tablename__ = 'summary'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     department = db.Column(db.String(100))
