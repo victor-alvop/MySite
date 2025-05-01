48f453e311bc9408b4c21582634130ec28788355
victor <vmalvaradop0@gmail.com>
2025-05-01 15:55:58 -0600

    Update index page

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import create_app
 import os
+from dotenv import load_dotenv
 
 app = create_app()
 
 if __name__ == "__main__":
     
     port = int(os.getenv('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
