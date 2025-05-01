f009f6178eb7cd2daef7c7a0f542aa56b433cf04
victor <vmalvaradop0@gmail.com>
2025-05-01 13:37:35 -0600

    Delete debug mode

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    # Usar el puerto de la variable de entorno 'PORT' que asigna Railway, o 5000 por defecto
+    
     port = int(os.getenv('PORT', 5000))
-
-    # Ejecutar la aplicación Flask en el puerto y dirección correcta
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
